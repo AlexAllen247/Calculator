fbfaf2292ddcca8c8777de15aedb40340a28d351
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2023-04-13 13:17:20 +0200

    continuing the calculator

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-let firstNumber = 0;
-let operator = "";
-let secondNumber = 0;
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) =>
+  num2 === 0 ? "Error: Cannot divide by zero" : num1 / num2;
 
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  if (num2 === 0) {
-    return "Error: Cannot divide by zero";
-  } else {
-    return num1 / num2;
-  }
-}
-
-function operate(operator, num1, num2) {
+const operate = (operator, num1, num2) => {
   switch (operator) {
     case "+":
       return add(num1, num2);
@@ -35,73 +17,59 @@ function operate(operator, num1, num2) {
     default:
       return null;
   }
-}
+};
 
 const display = document.querySelector(".display p");
 let displayValue = "0";
 
-function updateDisplay(value) {
-  if (displayValue === "0") {
-    displayValue = value;
-  } else {
-    displayValue += value;
-  }
+const updateDisplay = (value) => {
+  displayValue = displayValue === "0" ? value : displayValue + value;
   display.textContent = displayValue;
-}
+};
 
-const numberButtons = document.querySelectorAll(
-  ".buttons button:not(.operator)"
-);
-numberButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    updateDisplay(button.textContent);
+document
+  .querySelectorAll(".buttons button:not(.operator)")
+  .forEach((button) => {
+    button.addEventListener("click", () => updateDisplay(button.textContent));
   });
-});
 
-const operatorButtons = document.querySelectorAll(".buttons button.operator");
 let previousValue = null;
 let selectedOperator = null;
-let currentNumber = "0";
+let chainOperations = false;
 
-operatorButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    previousValue = parseFloat(displayValue);
+document.querySelectorAll(".buttons button.operator").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (previousValue !== null && !chainOperations) {
+      const currentNumber = parseFloat(displayValue);
+      const result = operate(selectedOperator, previousValue, currentNumber);
+      displayValue = result.toFixed(2);
+      display.textContent = parseFloat(displayValue);
+      previousValue = result;
+    } else {
+      previousValue = parseFloat(displayValue);
+    }
+
     selectedOperator = button.textContent;
     displayValue = "0";
-    currentNumber = "0";
+    chainOperations = true;
   });
 });
 
-const equalsButton = document.querySelector(".equals");
-equalsButton.addEventListener("click", function () {
-  if (!selectedOperator) {
-    return;
-  }
+document.querySelector(".equals").addEventListener("click", () => {
+  if (!selectedOperator) return;
 
-  currentNumber = parseFloat(displayValue);
+  const currentNumber = parseFloat(displayValue);
   const result = operate(selectedOperator, previousValue, currentNumber);
   displayValue = result.toFixed(2);
-  const finalResult = parseFloat(displayValue);
-  display.textContent = finalResult;
+  display.textContent = parseFloat(displayValue);
   previousValue = result;
   selectedOperator = null;
-  currentNumber = "0";
+  chainOperations = false;
 });
 
-const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", function () {
+document.querySelector(".clear").addEventListener("click", () => {
   displayValue = "0";
   previousValue = null;
   selectedOperator = null;
-  currentNumber = "0";
-  display.textContent = displayValue;
-});
-
-const backspaceButton = document.querySelector(".backspace");
-backspaceButton.addEventListener("click", function () {
-  displayValue = displayValue.slice(0, -1);
-  if (displayValue.length === 0) {
-    displayValue = "0";
-  }
   display.textContent = displayValue;
 });
